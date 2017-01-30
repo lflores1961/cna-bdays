8f10e6354f8dcc241bf557417792b90b8dcf5650
Fernando Flores <fernando.flores1961@gmail.com>
2017-01-29 18:31:23 -0600

    Add tests for static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,21 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+def setup
+  @base_title = "Colegio de Notarios"
+end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Inicio | #{@base_title}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Ayuda | #{@base_title}"
   end
 
 end
