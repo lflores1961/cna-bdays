471dcf81c9f87b5df6bc2527613ca7b4be5b2fd7
lflores1961 <fernando.flores1961@gmail.com>
2021-04-28 13:53:29 -0500

    Fixed 'The from object must be provided for every email send.' sendGrid gem error

--- app/mailers/member_mailer.rb
+++ app/mailers/member_mailer.rb
@@ -1,6 +1,6 @@
 class MemberMailer < ApplicationMailer
-  require 'sendgrid-ruby'
-  include SendGrid
+  # require 'sendgrid-ruby'
+  # include SendGrid
   
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
